5e48736c5a397eb575756a70b497462ca3fc7030
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2023-09-02 23:17:14 +0700

    add copyright to every files and composer.json

--- src/DirectPropertyAccessor.php
+++ src/DirectPropertyAccessor.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of rekalogika/direct-property-access package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
 namespace Rekalogika\DirectPropertyAccess;
 
 use Symfony\Component\PropertyAccess\Exception\InvalidArgumentException;
--- src/Symfony/DependencyInjection/RekalogikaPropertyAccessExtension.php
+++ src/Symfony/DependencyInjection/RekalogikaPropertyAccessExtension.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of rekalogika/direct-property-access package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
 namespace Rekalogika\DirectPropertyAccess\Symfony\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
